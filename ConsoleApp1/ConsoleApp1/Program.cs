bca4687a4f778b5d9908767a4a00f0e68edfbe2b
Prakash Tiwari <prakashtiwari.net@gmail.com>
2024-05-12 20:58:11 +0530

    Ice cream

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProblemSolution.MaxInWindow;
+
+Console.WriteLine("Hello, World!");
+MaxWindow maxWindow = new MaxWindow();
+var result = maxWindow.SlidingMaximum(new List<int> { 1, 3, -1, -3, 5, 3, 6, 7 }, 3);
+foreach (var v in result)
+{
+    Console.WriteLine(v);
+}
+Console.ReadLine();
